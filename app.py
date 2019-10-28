dbe27f2ce5c9b7e38979f274a96c0d187fc6f6ac
bob-crutchley <bob.crutchley96@gmail.com>
2019-10-28 10:40:12 +0000

    changed db user and pass

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ app = Flask(__name__, static_url_path='')
 
 def get_connection():
     return pymysql.connect(host=os.getenv("MYSQL_HOST"),
-                             user=os.getenv("root"),
-                             password=os.getenv("MYSQL_ROOT_PASSWORD"),
+                             user=os.getenv("MYSQL_USER"),
+                             password=os.getenv("MYSQL_PASSWORD"),
                              db=os.getenv("MYSQL_DATABASE"),
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor)
